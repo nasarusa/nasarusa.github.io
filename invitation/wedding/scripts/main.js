ebda282111a9a53d139a7c91b81040d785a5d20c
nasarusa <97671542+nasarusa@users.noreply.github.com>
2022-01-18 16:18:49 +0900

    Add files via upload

---
+++ invitation/wedding/scripts/main.js
@@ -0,0 +1,66 @@
+// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function () {
+    $("#map-image").on("click")
+    {
+    }
+
+    $('#go-to-top').click(function () {
+        $('html,body').animate({scrollTop: 0}, 400);
+        return false;
+    });
+})
+
+function copyAccount(val) {
+
+  var copyText = val;
+  var el = document.createElement('textarea');
+  el.value = copyText;
+  el.setAttribute('readonly', '');
+  el.style = {
+      position: 'absolute',
+      left: '-9999px'
+  };
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+
+  alert("복사되었습니다:)");
+}
+
+// Smooth scroll for links with hashes
+$("a.smooth-scroll").click(function (event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $("html, body").animate(
+                {
+                    scrollTop: target.offset().top
+                },
+                1000,
+                function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    }
+                }
+            );
+        }
+    }
+});
